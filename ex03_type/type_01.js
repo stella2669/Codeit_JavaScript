cacb1868904587335bbbdf94826495afb7fcb234
stella2669 <stella2669@gmail.com>
2020-11-13 15:06:02 +0900

    숫자형

---
+++ ex03_type/type_01.js
@@ -0,0 +1,17 @@
+// 거듭제곱
+console.log(2 ** 3);
+
+// 나머지
+console.log(7 % 3);
+
+// 덧셈
+console.log(1 + 8);
+
+// 뺄셈
+console.log(6 - 7);
+
+// 곱셈
+console.log(2 * 3 * 2);
+
+// 나눗셈
+console.log(5 / 2);

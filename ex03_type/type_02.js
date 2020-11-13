64d19f82a2f0109523fb46f3bc15700727bcd910
stella2669 <stella2669@gmail.com>
2020-11-13 15:14:47 +0900

    typeof 연산자

---
+++ ex03_type/type_02.js
@@ -0,0 +1,13 @@
+//typeof 연산자
+let name = 'Codeit';
+function sayHello(){
+  console.log('Hello');
+};
+
+console.log(typeof 2); //number
+console.log(typeof name); // string
+console.log(typeof sayHello); // function
+console.log(typeof typeof (3 + 5)); // string
+console.log(typeof true); // boolean
+
+

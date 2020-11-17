fd65af0a6cf376ca1fa9a83e299a9621275aec29
stella2669 <stella2669@gmail.com>
2020-11-17 11:24:18 +0900

    템플릿 문자열

---
+++ ex03_type/type_05.js
@@ -0,0 +1,21 @@
+// 템플릿 문자열 (template strings)
+// template : 일정한 틀, 형식
+
+let year = 2018;
+let month = 3;
+let day = 11;
+
+console.log('생년월일은 ' + year + '년' + month +'월' + day +'일 입니다.');
+
+console.log(`생년월일은 ${year}년 ${month}월 ${day}일 입니다.`);
+
+
+// 함수 활용
+let myNumber = 3;
+
+function getTwice(x) {
+  return x * 2;
+}
+
+console.log(`${myNumber}의 두배는 ${getTwice(myNumber)}입니다.`);
+
